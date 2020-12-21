f413cd9980cbcd5b5b84e616dc661e50c6952c25
Able <abl3theabove@gmail.com>
2020-12-20 20:53:00 -0600

    Yeah

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,6 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+pub mod chat;
+
 pub struct User {
     pub id: i32,
     pub username: String,
 }
-pub mod chat;
